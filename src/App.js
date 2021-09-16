5fbbf4fd36583ef76cfe8ad47f80aec752b3381e
MarianBadea <badea.alexandrumarian@gmail.com>
2021-09-16 11:56:21 +0300

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import CoffeeGrid from './components/CoffeeGrid/CoffeeGrid';
 import { coffees } from '../src/mock-data/coffees.json'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+    <h1 className="app__title">CoffeeShop</h1>
         <CoffeeGrid coffees={ coffees } />
     </div>
   );
