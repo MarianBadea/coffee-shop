3ed4c719ce0630accec07a8df5aba15063308ad4
MarianBadea <badea.alexandrumarian@gmail.com>
2021-09-16 13:09:17 +0300

    Ghange Links paths

--- src/pages/NavBar/NavBar.jsx
+++ src/pages/NavBar/NavBar.jsx
@@ -10,10 +10,10 @@ function NavBar(params) {
             <Link to="/coffees" className="navbar__link">
                 <p>Shop</p>
             </Link>
-            <Link to="/coffees" className="navbar__link">
+            <Link to="/blog" className="navbar__link">
                 <p>Blog</p>
             </Link>
-            <Link to="/coffees" className="navbar__link">
+            <Link to="/contact" className="navbar__link">
                 <p>Contact</p>
             </Link>
         </div>
