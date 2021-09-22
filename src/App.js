2d476693ec2435f764dfa4661a50e23af3a5bfdd
MarianBadea <badea.alexandrumarian@gmail.com>
2021-09-22 12:30:10 +0300

    Create Modal schelethon and logics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CoffeeGrid from './components/CoffeeGrid/CoffeeGrid';
@@ -6,12 +6,25 @@ import NavBar from './pages/NavBar/NavBar';
 import Home from './pages/Home/Home';
 import { coffees } from '../src/mock-data/coffees.json'
 import Footer from './pages/Footer/Footer';
+import Modal from './components/Modal/Modal';
 
 function App() {
+  const [ openModal, setOpenModal ] = useState(false)
+
   return (
     <Router>
       <div className="app">
         <h1 className="app__title">CoffeeShop</h1>
+        <button
+          onClick={ () => setOpenModal(true) }
+        >
+          Baschet
+        </button>
+        <Modal 
+          title="Shopping Baschet"
+          isOpen={ openModal }
+          onClose={ () => setOpenModal(false) }
+        />
         <div className="app__content">
           <NavBar />
             <div>
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,57 @@
-function Modal(params) {
+import './Modal.scss'
+import PropTypes from 'prop-types';
+
+function getHeader(onClose, headerContent, title) {
+    return (
+        <div className="cf-modal__header">
+            <div className="cf-modal__header-left">
+                { headerContent }
+            </div>
+            <div className="cf-modal__header-center">
+                <h1>{ title }</h1>
+            </div>
+            <div className="cf-modal__header-right">
+                <button className="cf-modal_close-button" type="button" onClick={ onClose }>
+                    Close
+                </button>
+            </div>
+        </div>
+    );
+}
+
+function Modal({
+    isOpen,
+    onClose,
+    title,
+    headerContent,
+    children,
+    footerContent,
+  }) {
     return(
-        <div>
-            <p>Modal</p>
+        <div className={`cf-modal-wrapper ${ isOpen ? '' : '--hidden' }`}>
+        <div className="cf-modal-overlay" />
+            <div className="cf-modal">
+                <div className="cf-modal__content">
+                    {getHeader(onClose, headerContent, title)}
+                    <p>Body</p>
+                    <p>Footer</p>
+                </div>
+            </div>
         </div>
     )
 }
+
+Modal.propTypes = {
+    title: PropTypes.string.isRequired,
+    isOpen: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired,
+    headerContent: PropTypes.node,
+    footerContent: PropTypes.node,
+  };
+  
+  Modal.defaultProps = {
+    headerContent: null,
+    footerContent: null,
+  };
 
 export default Modal;
